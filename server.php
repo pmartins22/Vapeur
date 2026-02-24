271af7ebd2ee542efabc05bcbbfbae35da38704e
Pedro Martins <pedrohenrique.costamartins@ynov.com>
2026-02-24 11:38:00 +0100

    feat(server):
    - updated routes

--- server.php
+++ server.php
@@ -3,12 +3,13 @@
 require_once 'router.php';
 
 $router = new Router();
-$router->add('/home',     'public/home.html');
-$router->add('/profile/', 'public/profile.html');
-$router->add('/admin', 'public/admin.html');
-$router->add('/game/', 'public/game.html');
-$router->add('/login', 'public/login.html');
-$router->add('/signin', 'public/signin.html');
+$router->add('/',     'public/index.php');
+$router->add('/home',     'public/home.php');
+$router->add('/profile/', 'public/profile.php');
+$router->add('/admin', 'public/admin.php');
+$router->add('/game/', 'public/game.php');
+$router->add('/login', 'public/login.php');
+$router->add('/signin', 'public/signin.php');
 
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 $router->resolve($uri);
