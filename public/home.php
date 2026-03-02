2789feaab96d85f9a8963ab80459bd84a2b15c46
Pedro Martins <pedrohenrique.costamartins@ynov.com>
2026-03-02 17:24:07 +0100

    hotfix(home.php):
    - fixed css link

--- public/home.php
+++ public/home.php
@@ -72,7 +72,7 @@ function getTagColor($category) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vapeur - Accueil</title>
-    <link rel="stylesheet" href="/home.css">
+    <link rel="stylesheet" href="home.css">
 </head>
 <body>
 <div class="wrapper">
